0b377de893c17975801d3eb980e09e58069b9822
iuvelirGE <92124599+iuvelirGE@users.noreply.github.com>
2023-03-11 02:43:22 +0200

    Create API Request

--- Rick&Morty/API Clients/RMRequest.swift
+++ Rick&Morty/API Clients/RMRequest.swift
@@ -9,5 +9,67 @@ import Foundation
 
 /// Объект, представляющий одиночный вызов API
 final class RMRequest {
+    /// API Constant
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    /// Конечная точка
+    private let endpoint: RMEndpoint
+    
+    /// Компоненты пути для АПИ
+    private let pathComponents: Set<String>
+    
+    /// Запросы
+    private let quaryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api  request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !quaryParameters.isEmpty {
+            string += "?"
+            let argumentString = quaryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    /// Вычисленный и сконструированный адрес API
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    /// HTTP Method
+    public let  httpMethod = "GET"
+    
+    // MARK: - Public
+    
+    /// Запрос
+    /// - Parameters:
+    ///   - endpoint: Endpoint
+    ///   - pathComponents: Collection path
+    ///   - quaryParameters: Collection of quary
+    public init(
+        endpoint: RMEndpoint,
+        pathComponents: Set<String> = [],
+        quaryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.quaryParameters = quaryParameters
+    }
 }
--- Rick&Morty/Controllers/Core/RMCharacterViewController.swift
+++ Rick&Morty/Controllers/Core/RMCharacterViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 /// Контроллер для отображения и поиска Characters
 final class RMCharacterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            quaryParameters: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        print(request.url)
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+            
+            }
+        }
     }
 
-    
-    
-}
